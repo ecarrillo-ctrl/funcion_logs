7255232fa3d13b42bf3837d8c52f1abb99ebc0f6
Eric Daniel Carrillo Canizales <ecarrillo@CORPORATIVOAGROAMERICA.CORP>
2026-04-24 13:48:53 -0600

    Primer commit - versión inicial con funcion_logs y dashboard_logs

---
+++ Package.js
@@ -0,0 +1,20 @@
+{
+  "name": "gestion-cloudflare-api",
+  "version": "1.0.0",
+  "description": "API para gestion de Cloudflare y auditorias",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0",
+    "cors": "^2.8.5",
+    "xlsx": "^0.18.5",
+    "pdfkit": "^0.15.0",
+    "googleapis": "^126.0.0"
+  },
+  "engines": {
+    "node": "20.x"
+  }
+}
